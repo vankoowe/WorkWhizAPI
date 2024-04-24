0d886bac24364e261cc7e544aeccc5e1e0dd1c8e
Ivan Gamov <idgamov@gmail.com>
2024-04-24 14:42:30 +0300

    Create Auth API Requests

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const registerRouter = require("./routes/register");
+const loginRouter = require("./routes/login");
+const express = require("express");
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const PORT = process.env.PORT || 5010;
+const conString = process.env.CONNECTION_STRING || "localhost";
+const app = express();
+
+app.use(express.json());
+app.use("/api", registerRouter);
+app.use("/api", loginRouter);
+
+mongoose.connect(conString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("strictQuery", true);
+
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
